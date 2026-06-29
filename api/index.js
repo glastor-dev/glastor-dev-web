b0b6ac040f6169be5d77abc0df4bd5d2eeabbe8f
Glastor Dev <dev@glastor.com>
2026-06-29 20:33:28 -0300

    chore: add try-catch wrapper around server load to expose boot error to browser

--- api/index.js
+++ api/index.js
@@ -1,2 +1,21 @@
-const server = require('../server');
-module.exports = server.default || server;
+let serverApp;
+let initError;
+
+try {
+  const serverModule = require('../server');
+  serverApp = serverModule.default || serverModule;
+} catch (e) {
+  initError = {
+    message: e.message,
+    stack: e.stack,
+    name: e.name
+  };
+}
+
+module.exports = (req, res) => {
+  if (initError) {
+    res.status(200).json({ diagnosticError: initError });
+  } else {
+    serverApp(req, res);
+  }
+};
